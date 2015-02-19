07c49e40873d859a70cb30a99e6470022359fbaa
Paul <paul.irwin90@gmail.com>
2015-02-19 11:39:09 -0700

    Testing

---
+++ OpenCV Tests/OpenCV Tests/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCV Tests
+//
+//  Created by Paul Irwin on 2/19/15.
+//  Copyright (c) 2015 Paul Irwin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code 
+    std::cout << "Hello, World!\n";
+    return 0;
+}
